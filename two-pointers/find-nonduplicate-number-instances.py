ea70dd7935b9f86c817f72d9439ee8913eff2b85
J-Bills <billsjamal@gmail.com>
2025-01-16 01:39:43 -0800

    problem setup

---
+++ two-pointers/find-nonduplicate-number-instances.py
@@ -0,0 +1,20 @@
+class Solution:
+    def moveElements(self, arr):
+        """
+        Given an array of sorted numbers, move all non-duplicate number instances at the 
+        beginning of the array in-place. 
+        The non-duplicate numbers should be sorted and you should not use any extra space 
+        so that the solution has constant space complexity i.e., O(1) .
+
+        Move all the unique number instances at the beginning of the array and 
+        after moving return the length of the subarray that has no duplicate in it.
+        """
+        # TODO: Write your code here
+        
+        return 1
+    
+    
+sol = Solution()
+arr = [2, 3, 3, 3, 6, 9, 9]
+result = sol.moveElements(arr)
+print(f'result: {result}')
