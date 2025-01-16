c2e3968d7e5a475cc4dde0cf8841e7db661699ef
J-Bills <billsjamal@gmail.com>
2025-01-16 01:17:12 -0800

    setting up problem

---
+++ two-pointers/pair-with-target-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, arr, target_sum):
+        """Given an array of numbers sorted in ascending order and a target sum,
+        find a pair in the array whose sum is equal to the given target.
+
+        Write a function to return the indices of the two numbers (i.e. the pair) 
+        such that they add up to the given target. If no such pair exists return [-1, -1]."""
+        # TODO: Write your code here
+        
+        return [-1, -1]
+    
+sol = Solution()
+arr = [1, 2, 3, 4, 6]
+target_tum = 6
+sol.search()
